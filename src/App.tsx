019e56306b075d30a87371524bda31eceeb316e9
kvils <flugnet@gmail.com>
2020-04-12 18:46:01 +0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './footer.scss'
+import Social from '../social/social'
+import Logo from '../../assets/images/logoFoot.png'
+
+export const Footer: React.FC = () => {
+    return (
+        <footer className="footer container flex-sb">
+            <div className="footer__item">
+                <a href="/"><img src={Logo} alt="Logo" /></a>
+		    </div>
+            <div className="footer__item">
+                <Social />
+            </div>
+            <div className="footer__item">
+                <div className="footer__copyright">All Rights Reserved 2018 BehindMars</div>
+            </div>
+	    </footer>
+    )
+}
+
+export default Footer;
