d85fedf4c8f4e28d0ff5719337690635691f2bac
kvils <flugnet@gmail.com>
2020-04-13 19:42:15 +0300

    fixed App.scss;

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import './App.scss'
-<<<<<<< HEAD
+import './App.scss';
 import Header from './components/header/header'
 import About from './components/about/about'
 import BredCrumb from './components/bredCrumb/bredCrumb'
 import Post from './components/post/post'
 import Related from './components/related/related'
-=======
 import Comments from './components/comments/comments'
->>>>>>> post/comments
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <div className="container">
         <Header />
         <About />
@@ -22,10 +18,7 @@ function App() {
         <BredCrumb />
         <Post />
         <Related />
-=======
-      <main className="main">
         <Comments />
->>>>>>> post/comments
       </main>
     </>
   );
