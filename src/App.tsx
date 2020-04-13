bd49aee47b6106a2ae4226e6fe739fe3a972bfca
kvils <flugnet@gmail.com>
2020-04-13 19:14:15 +0300

    fixet about link

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react'
 import './App.scss'
-<<<<<<< HEAD
 import Header from './components/header/header'
 import About from './components/about/about'
-=======
 import BredCrumb from './components/bredCrumb/bredCrumb'
->>>>>>> post/bred-crumb
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <div className="container">
         <Header />
         <About />
       </div>
-=======
       <main className="main">
         <BredCrumb />
       </main>
->>>>>>> post/bred-crumb
     </>
   );
 }
