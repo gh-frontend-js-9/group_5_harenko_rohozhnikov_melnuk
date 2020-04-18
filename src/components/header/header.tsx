8da89d4baa9c02e43979d7e1eb27c573fd4eab85
spektr001 <sanek777.28.03.2000@gmail.com>
2020-04-19 01:58:18 +0300

    fix burger-menu events

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.scss'
-import '../Header/burgerMenu/burgerMenu.scss'
+import '../header/burgerMenu/burgerMenu.scss'
 import Logo from '../../assets/images/logo.png'
 import Navigation from './navigation/navigation'
 // import 'https://code.jquery.com/jquery-3.1.1.min.js'
@@ -8,28 +8,46 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import BurgerMenu from './burgerMenu/burgerMenu';
 
-export const Header: React.FC = () => {
-    return (
-        <header className="header flex-sb">
-            <div className="header__item flex-sb">
-                <a className="header__link" href="/">
-                    <img className="header__logo logo" src={Logo} alt="Logo" />
-                </a>
-                <nav className="header__nav nav burger-menu">
-                    <Navigation />
-                    {/* <BurgerMenu/> */}
-                </nav>
-            </div>
-            <div className="header__item">
-                <FontAwesomeIcon icon={faSearch} className="header__search"/>
-                <div className="burger-menu" onClick={($event: any) => $event.target.classList.add('burger-menu__lines--active')}>
-                    <a href="#" className="burger-menu__button" >
-                        <span className="burger-menu__lines"></span>
+export class Header extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            isDisplay: true
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e: any) {
+        e.target.classList.toggle('burger-menu__lines--active')
+        this.setState((state: any) => ({
+            isDisplay: !state.isDisplay
+        }));
+    }
+
+    render() {
+        const style = this.state.isDisplay ? { display: 'none' } : { display: 'block' };
+        return (
+            <header className="header flex-sb">
+                <div className="header__item flex-sb">
+                    <a className="header__link" href="/">
+                        <img className="header__logo logo" src={Logo} alt="Logo" />
                     </a>
+                    <nav className="header__nav nav burger-menu">
+                        <Navigation display={style} />
+                        {/* <BurgerMenu /> */} 
+                    </nav>
                 </div>
-            </div>
-	    </header>
-    )
+                <div className="header__item">
+                    <FontAwesomeIcon style={style} icon={faSearch} className="header__search" />
+                    <div className="burger-menu" onClick={e => this.handleClick(e)}>
+                        <a href="#" className="burger-menu__button" >
+                            <span className="burger-menu__lines"></span>
+                        </a>
+                    </div>
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Header;
--- src/components/header/navigation/navigation.tsx
+++ src/components/header/navigation/navigation.tsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import './navigation.scss'
 
-export const Navigation: React.FC = () => {
-    return (
-        <ul className="menu flex-sa">
-            <li className="menu__item">
-                <a className="menu__link" href="/popular">Popular</a>
-            </li>
-            <li className="menu__item">
-                <a className="menu__link" href="/new">New</a>
-            </li>
-            <li className="menu__item">
-                <a className="menu__link" href="/post">Post</a>
-            </li>
-            <li className="menu__item">
-                <a className="menu__link" href="/readinglist">Reading list</a>
-            </li>
-            <li className="menu__item">
-                <a className="menu__link" href="/topics">Topics</a>
-            </li>
-            <li className="menu__item">
-                <a className="menu__link" href="/subscribe">Subscribe</a>
-            </li>
-            <li className="menu__item">
-                <a className="menu__link" href="/author">Author</a>
-            </li>
-        </ul>
-    )
+export class Navigation extends React.Component<any> {
+    constructor(props: any) {
+        super(props);
+    }
+    render() {
+        return (
+            <ul style={this.props.display} className="menu flex-sa">
+                <li className="menu__item">
+                    <a className="menu__link" href="/popular">Popular</a>
+                </li>
+                <li className="menu__item">
+                    <a className="menu__link" href="/new">New</a>
+                </li>
+                <li className="menu__item">
+                    <a className="menu__link" href="/post">Post</a>
+                </li>
+                <li className="menu__item">
+                    <a className="menu__link" href="/readinglist">Reading list</a>
+                </li>
+                <li className="menu__item">
+                    <a className="menu__link" href="/topics">Topics</a>
+                </li>
+                <li className="menu__item">
+                    <a className="menu__link" href="/subscribe">Subscribe</a>
+                </li>
+                <li className="menu__item">
+                    <a className="menu__link" href="/author">Author</a>
+                </li>
+            </ul>
+        )
+    }
 }
 
 export default Navigation;
