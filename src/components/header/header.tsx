a75af60aac7f96294f6d8f0b36e51dae205d7eb5
kvils <flugnet@gmail.com>
2020-04-18 20:14:37 +0300

    error remove click

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from 'react';
 import './header.scss'
+import '../Header/burgerMenu/burgerMenu.scss'
 import Logo from '../../assets/images/logo.png'
-import Navigation from '../navigation/navigation'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Navigation from './navigation/navigation'
+// import 'https://code.jquery.com/jquery-3.1.1.min.js'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import BurgerMenu from './burgerMenu/burgerMenu';
 
 export const Header: React.FC = () => {
     return (
         <header className="header flex-sb">
-            <div className="header__item">
-                <a href="/"><img src={Logo} alt="Logo" /></a>
+            <div className="header__item flex-sb">
+                <a className="header__link" href="/">
+                    <img className="header__logo logo" src={Logo} alt="Logo" />
+                </a>
+                <nav className="header__nav nav burger-menu">
+                    <Navigation />
+                    {/* <BurgerMenu/> */}
+                </nav>
             </div>
             <div className="header__item">
-                <Navigation />
-            </div>
-            <div className="header__item">
-                <FontAwesomeIcon icon={faSearch} />
+                <FontAwesomeIcon icon={faSearch} className="header__search"/>
+                <div className="burger-menu" onClick={($event: any) => $event.target.classList.add('burger-menu__lines--active')}>
+                    <a href="#" className="burger-menu__button" >
+                        <span className="burger-menu__lines"></span>
+                    </a>
+                </div>
             </div>
 	    </header>
     )
--- src/components/header/navigation/navigation.tsx
+++ src/components/header/navigation/navigation.tsx
@@ -3,12 +3,15 @@ import './navigation.scss'
 
 export const Navigation: React.FC = () => {
     return (
-        <ul className="menu menu flex-sa">
+        <ul className="menu flex-sa">
             <li className="menu__item">
                 <a className="menu__link" href="/popular">Popular</a>
             </li>
             <li className="menu__item">
                 <a className="menu__link" href="/new">New</a>
+            </li>
+            <li className="menu__item">
+                <a className="menu__link" href="/post">Post</a>
             </li>
             <li className="menu__item">
                 <a className="menu__link" href="/readinglist">Reading list</a>
@@ -18,6 +21,9 @@ export const Navigation: React.FC = () => {
             </li>
             <li className="menu__item">
                 <a className="menu__link" href="/subscribe">Subscribe</a>
+            </li>
+            <li className="menu__item">
+                <a className="menu__link" href="/author">Author</a>
             </li>
         </ul>
     )
