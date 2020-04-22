42e092d0d7dfa9ba110ac165d499d8e9131c11c7
kvils <flugnet@gmail.com>
2020-04-22 20:09:15 +0300

    fixed merge header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react'
-import './App.scss';
-<<<<<<< HEAD
-import Header from './components/blogPage/Header/Header'
-import ArticlesArea from './components/blogPage/ArticlesBlocks/ArticlesArea'
-import BottomBlocksArea from './components/blogPage/BottomBlocks/BottomBlocksArea'
-import Footer from './components/blogPage/Footer/Footer'
-=======
+import './App.scss'
 import Header from './components/Header/header'
->>>>>>> post/header
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Header />
-      <ArticlesArea />
-      <BottomBlocksArea />
-      <Footer />
-=======
       <div className="container">
         <Header  style="display: block"/>
       </div>
->>>>>>> post/header
     </>
   );
 }
 
-export default App
+export default App;
